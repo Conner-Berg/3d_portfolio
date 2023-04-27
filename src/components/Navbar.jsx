1cd06d14661d31aadb9ac6787f01f14696ca28c0
Conner-Berg <connerwberg@gmail.com>
2023-04-27 12:01:16 -0400

    Update name and portfolio layout on the left

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,8 +29,8 @@ const Navbar = () => {
 						alt="logo"
 						className="w-9 h-9 object-contain"
 					/>
-					<p className="text-white text-[18px] font-bold cursor-pointer">
-						Conner Berg{" "}
+					<p className="text-white text-[18px] font-bold cursor-pointer flex">
+						Conner Berg &nbsp;
 						<span className="sm:block hidden">| Portfolio</span>
 					</p>
 				</Link>
