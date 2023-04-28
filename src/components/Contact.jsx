be83f1d66b86f6db2a4543901397014b48099afa
Conner-Berg <connerwberg@gmail.com>
2023-04-28 15:51:11 -0400

    Add complete functionality with email service

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,9 +12,47 @@ const Contact = () => {
 	const [form, setForm] = useState({ name: "", email: "", message: "" });
 	const [loading, setLoading] = useState(false);
 
-	const handleChange = (e) => {};
+	const handleChange = (e) => {
+		const { name, value } = e.target;
 
-	const handleSubmit = (e) => {};
+		setForm({ ...form, [name]: value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
+
+		emailjs
+			.send(
+				"service_d5hcred",
+				"template_awnyyir",
+				{
+					from_name: form.name,
+					to_name: "Conner",
+					from_email: form.email,
+					to_email: "contact@connerberg.com",
+					message: form.message,
+				},
+				"gpAjfAMT4KiqqVg4M"
+			)
+			.then(
+				() => {
+					setLoading(false);
+					alert(
+						"Thank you. I will get back to you as soon as possible."
+					);
+
+					setForm({ name: "", email: "", message: "" });
+				},
+				(error) => {
+					setLoading(false);
+
+					console.log(error);
+
+					alert("Something went wrong.");
+				}
+			);
+	};
 
 	return (
 		<div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
